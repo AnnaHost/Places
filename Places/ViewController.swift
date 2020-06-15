fe9a1040a54662131cccf3182174ce7f97c9b856
AnnaHost <annahost1488@yandex.ru>
2020-06-15 16:42:33 +0500

    Initial Commit

---
+++ Places/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Places
+//
+//  Created by Анна Гареева on 15.06.2020.
+//  Copyright © 2020 Anna Gareeva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
